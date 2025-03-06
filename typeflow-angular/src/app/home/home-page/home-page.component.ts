212bf93adb5e9db88a6c48fa0794356f961c2aa6
Anastasiia Tiutiun <tiutiunanastasia@gmail.com>
2025-03-06 23:52:16 +0200

    add sth to home page

--- typeflow-angular/src/app/home/home-page/home-page.component.ts
+++ typeflow-angular/src/app/home/home-page/home-page.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../shared/header/header.component';
 import { TimerComponent } from '../../../shared/timer/timer.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
 	selector: 'tf-home-page',
-	imports: [HeaderComponent, TimerComponent, MatIconModule],
+	imports: [HeaderComponent, TimerComponent, MatIconModule, MatInputModule, MatFormFieldModule, FormsModule, MatButtonModule],
 	templateUrl: './home-page.component.html',
 	styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
-
+	public text: string = "So call me when the work looks bleak. I love you, but it's hard to believe. With every day we'll start to see. The rest is metamodernity";
+	public userText: string = "";
 }
